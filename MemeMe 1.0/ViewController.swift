1d6be6edddfeb1db214e216a8538aa01bd7a812f
Frances Koo <happyengr1@hotmail.com>
2020-09-14 22:30:49 -0700

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Frances Koo on 9/14/20.
+//  Copyright © 2020 happyengr1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
